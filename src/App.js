8b0b3d8588f6abb185d1ae5cf57bb8eef9bf4c82
Umi uzli aulia <umi99kirei@gmail.com>
2022-08-29 07:36:50 +0700

    perbaikan

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import Header from './component/header/Header';
 import About from './component/about/About';
 import Nav from './component/nav/Nav';
 import Experience from './component/experience/Experience';
-import Services from './component/services/Services';
 import Portofolio from './component/portofolio/Portofolio';
-import Testimonials from './component/testimonials/Testimonials';
 import Contact from './component/contact/Contact';
 import Footer from './component/footer/Footer';
 
 function App() {
   return (
-    <>
+    <div>
       <Header/>
       <Nav/>
       <About/>
       <Experience/>
-      <Services/>
       <Portofolio/>
-      <Testimonials/>
       <Contact/>
       <Footer/>
-    </>
+    </div>
   );
 }
 
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import './footer.css'
+import {FiInstagram} from 'react-icons/fi'
+import {BsFacebook} from 'react-icons/bs'
+import {AiFillTwitterCircle} from 'react-icons/ai'
 
 const Footer = () => {
     return(
-        <section id='footer'>Footer</section>
+        <footer id='footer'>
+            <a href="#" className="fppter_logo">Miumy</a>
+            <ul className="permalinks">
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#portofolio">Portofolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div className="footer_sosials">
+                <a href="https://www.instagram.com/miumy2805/"><FiInstagram/></a>
+                <a href="https://mobile.facebook.com/umiaulia.kirei"><BsFacebook/></a>
+                <a href="https://twitter.com/umiuzliaulia1"><AiFillTwitterCircle/></a>
+            </div>
+        </footer>
     )
 }
 export default Footer
